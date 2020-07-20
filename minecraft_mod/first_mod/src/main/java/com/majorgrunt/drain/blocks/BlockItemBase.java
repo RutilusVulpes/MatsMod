7a974e1937d9c2e8b429b0a1389c10af18524e74
Mathew <mathewgutierrez00@gmail.com>
2020-07-19 23:47:56 -0600

    work

--- minecraft_mod/first_mod/src/main/java/com/majorgrunt/drain/blocks/BlockItemBase.java
+++ minecraft_mod/first_mod/src/main/java/com/majorgrunt/drain/blocks/BlockItemBase.java
@@ -8,8 +8,7 @@ import net.minecraft.item.Item;
 
 public class BlockItemBase extends BlockItem{
 
-	
-	
+
 	public BlockItemBase(Block block) {
 		super(block, new Item.Properties().group(Drain.TAB));
 
--- minecraft_mod/first_mod/src/main/java/com/majorgrunt/drain/blocks/RubyOre.java
+++ minecraft_mod/first_mod/src/main/java/com/majorgrunt/drain/blocks/RubyOre.java
@@ -1,11 +1,15 @@
 package com.majorgrunt.drain.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.OreBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ToolType;
 
-public class RubyOre extends Block{
+public class RubyOre extends OreBlock{
 
 	public RubyOre() {
 		super(Block.Properties.create(Material.EARTH)
@@ -14,7 +18,11 @@ public class RubyOre extends Block{
 			.sound(SoundType.STONE)
 			.harvestTool(ToolType.PICKAXE)
 		);
-		
+	}
+	
+	@Override
+	public int getExpDrop(BlockState state, IWorldReader reader, BlockPos pos, int fortune, int silktouch) {
+		return 1;
 	}
 
 }
